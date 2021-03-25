b891a84dfb348c947f37ef2a7fe430859cd8b75f
j-freddy <jtf.imperial@gmail.com>
2021-03-25 12:40:38 +0000

    Weekday field, started building GUI

--- day_weather.js
+++ day_weather.js
@@ -1,24 +1,31 @@
 class DayWeather {
     static descriptions = ["Clear", "Clouds", "Snow", "Rain", "Drizzle", "Thunderstorm", "Overcast"];
+    static weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     description;
     minTemp;
     maxTemp;
+    weekday;
 
     isToday;
     currentTemp;
 
-    constructor(description, minTemp, maxTemp, isToday = false, currentTemp = -99) {
+    constructor(description, minTemp, maxTemp, weekdayIndex, isToday = false, currentTemp = -99) {
         this.description = description;
         this.minTemp = minTemp;
         this.maxTemp = maxTemp;
+        this.weekday = this.getWeekdayAbbre(weekdayIndex);
         this.isToday = isToday;
         this.currentTemp = currentTemp;
         this.updateDescription();
     }
 
+    getWeekdayAbbre(index) {
+        return DayWeather.weekdays[index].substring(0, 3);
+    }
+
     updateDescription() {
-        //OpenWeatherMap gives a long list of descrptions synonymous to 'overcast'
+        //OpenWeatherMap gives a long list of descriptions synonymous to 'overcast'
         if(!DayWeather.descriptions.find(word => this.description === word)) {
             this.description = "Overcast";
         }
--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const DOMAIN_NAME = "https://api.openweathermap.org";
 
 //Update this to match user location
-let latitude = 51.5148;
-let longitude = -0.1469;
-let unit = "metric";
+const latitude = 51.5148;
+const longitude = -0.1469;
+const unit = "metric";
+
+const mainContainer = document.getElementById("main");
 
 let req = `${DOMAIN_NAME}/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=${unit}&appid=${TOKEN}`;
+
+/*
+    index = 0 -> Today
+    index = 1 -> Tomorrow
+*/
+function getWeekDayIndex(index) {
+    let d = new Date();
+    return (d.getDay() + index) % 7;
+}
 
 function parseData(data) {
     let daysWeather = [];
@@ -20,22 +31,40 @@ function parseData(data) {
 
         if(isToday) {
             currentTemp = data.current.temp;
-            daysWeather.push(new DayWeather(description, minTemp, maxTemp, true, currentTemp));
+            daysWeather.push(new DayWeather(description, minTemp, maxTemp, getWeekDayIndex(i), true, currentTemp));
         } else {
-            daysWeather.push(new DayWeather(description, minTemp, maxTemp));
+            daysWeather.push(new DayWeather(description, minTemp, maxTemp, getWeekDayIndex(i)));
         }
     });
 
     return daysWeather;
 }
 
-//Fetch data
+function displayDay(dayWeather) {
+    //Container
+    let elem = document.createElement("div");
+    //Quarter width
+    elem.className = "col-md-3";
+
+    let info = document.createElement("p");
+    info.innerHTML =
+        "Description: " + dayWeather.description + "<br />" +
+        "Date: " + dayWeather.weekday + "<br />" +
+        "Min: " + dayWeather.minTemp + "<br />" +
+        "Max: " + dayWeather.maxTemp;
+    elem.appendChild(info);
+
+    mainContainer.appendChild(elem);
+}
+
 fetch(req)
     .then(response => response.json())
     .then(data => {
         let filteredData = parseData(data);
         console.log(filteredData);
+
+        filteredData.forEach(day => displayDay(day));
     })
-    .catch(_ => {
-        console.log("Error in fetching data");
+    .catch(e => {
+        throw e;
     });
